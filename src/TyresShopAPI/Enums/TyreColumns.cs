1e403bc5d805cf262e27a2c14167ebbba8b540bb
jakub <jakubstankowski@protonmail.com>
2023-08-11 15:26:56 +0200

    #18 implement sort by column, and default sorting

---
+++ src/TyresShopAPI/Enums/TyreColumns.cs
@@ -0,0 +1,12 @@
+﻿namespace TyresShopAPI.Enums
+{
+    public enum TyreColumns
+    {
+        Id = 0,
+        Model = 1,
+        ProducerName = 2,
+        ProductionYear = 3,
+        TyresTypeNane = 4,
+        Price = 5,
+    }
+}

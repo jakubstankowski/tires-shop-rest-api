edea34e1e77c8ba41fd976a08d946ae0f2a289ca
Michal Kalinowski <kalinowskimichal638@gmail.com>
2023-08-18 12:45:49 +0200

    #14

--- src/TyresShopAPI.Domain/Entities/Cart.cs
+++ src/TyresShopAPI.Domain/Entities/Cart.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TyresShopAPI.Domain.Enums;
 
 namespace TyresShopAPI.Domain.Entities
 {
@@ -13,6 +14,7 @@ namespace TyresShopAPI.Domain.Entities
         public decimal PriceTotal { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime Updated { get; set; } = DateTime.Now;
+        public OrderStatus OrderStatus { get; set; }
         public List<CartItem> CartItems { get; set; }
     }
 }
---
+++ src/TyresShopAPI.Infrastructure/Persistance/Migrations/20230818104431_Cart4.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TyresShopAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cart4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderStatus",
+                table: "Cart",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderStatus",
+                table: "Cart");
+        }
+    }
+}

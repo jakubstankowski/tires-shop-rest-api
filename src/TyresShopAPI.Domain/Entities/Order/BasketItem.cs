a964a9e7d659beb253148de313b3569a7e6ba228
jakub <jakubstankowski@protonmail.com>
2023-08-18 13:26:11 +0200

    #14 add order entity

---
+++ src/TyresShopAPI.Domain/Entities/Order/BasketItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TyresShopAPI.Domain.Entities.Order
+{
+    public class BasketItem : BaseModel
+    {
+        public Tyre Tyre { get; set; }
+        public int TyreId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- src/TyresShopAPI.Infrastructure/Persistance/Context.cs
+++ src/TyresShopAPI.Infrastructure/Persistance/Context.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using TyresShopAPI.Domain.Entities;
 using TyresShopAPI.Domain.Entities.Customers;
+using TyresShopAPI.Domain.Entities.Order;
 
 namespace TyresShopAPI.Infrastructure.Persistance
 {
@@ -18,6 +19,11 @@ namespace TyresShopAPI.Infrastructure.Persistance
         public DbSet<Customer> Customers { get; set; } = null!;
 
         public DbSet<Address> Adresses { get; set; } = null!;
+
+        public DbSet<Order> Orders { get; set; } = null!;
+
+        public DbSet<BasketItem> Basket { get; set; } = null!;
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

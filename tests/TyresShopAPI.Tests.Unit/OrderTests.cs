bf44d739ec2d972ebf748c1b2c5815afc6086607
jakub <jakubstankowski@protonmail.com>
2023-09-08 16:14:34 +0200

    Remove unused tests

--- tests/TyresShopAPI.Tests.Unit/OrderTests.cs
+++ tests/TyresShopAPI.Tests.Unit/OrderTests.cs
@@ -11,20 +11,5 @@ namespace TyresShopAPI.Tests.Unit
     public class OrderTests
     {
 
-        [Fact]
-        public void CalculateSubTotal_WithEmptyCart_ShouldReturnZero()
-        {
-            // Arrange
-            List<CartView> emptyCart = new List<CartView>();
-            decimal deliveryPrice = 10.0m; // Example delivery price
-
-            // Act
-            decimal result = CalculateSubTotal(emptyCart, deliveryPrice);
-
-            // Assert
-            Assert.Equal(10.0m, result); // The result should be equal to the delivery price
-        }
-
-
     }
 }
